1caa8e625a4256e38ca6c153d082d04b0d922717
MariaNabil <maria.nabilfcis@hotmail.com>
2021-07-15 12:09:26 +0200

    Add React Query

---
+++ src/screens/Details/Authors.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {useQuery} from 'react-query';
+import {smallFontSize} from '../../constants/styles';
+
+function Authors(props) {
+  console.log('props', props.query);
+  const fetchAuthor = async () =>
+    await (await fetch(`https://openlibrary.org${props.query}.json`)).json();
+
+  const {data, error, status} = useQuery('users', fetchAuthor);
+
+  if (status === 'success') console.log('data', data);
+  return (
+    <View>
+      <View>
+        {status === 'error' && <Text>--</Text>}
+
+        {status === 'loading' && <Text>Loading...</Text>}
+
+        {status === 'success' && (
+          <Text style={{fontSize: smallFontSize}}>{data.name}</Text>
+        )}
+      </View>
+    </View>
+  );
+}
+
+export default Authors;
--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -6,50 +6,63 @@ import {
   smallFontSize,
   smallPadding,
 } from '../../constants/styles';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import Authors from './Authors';
 
 const Details = props => {
   const item = props.route?.params?.item;
 
+  const queryClient = new QueryClient();
+
   return (
-    <View
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: smallPadding,
-        flex: 1,
-        backgroundColor: bgColor,
-      }}>
-      <StatusBar backgroundColor={mainColor} translucent={false} />
-      <Text
+    <QueryClientProvider client={queryClient}>
+      <View
         style={{
-          fontWeight: 'bold',
-          fontSize: largeFontSize,
-          alignSelf: 'center',
+          paddingHorizontal: 20,
           paddingVertical: smallPadding,
+          flex: 1,
+          backgroundColor: bgColor,
         }}>
-        {item.title}
-      </Text>
-      <Text
-        style={{
-          fontSize: smallFontSize,
-          paddingVertical: smallPadding,
-        }}>
-        {`Published By : ${item.publishers.join(' and ')}`}
-      </Text>
-      <Text
-        style={{
-          fontSize: smallFontSize,
-          paddingVertical: smallPadding,
-        }}>
-        {`Published At : ${item.publish_date}`}
-      </Text>
-      <Text
-        style={{
-          fontSize: smallFontSize,
-          paddingVertical: smallPadding,
-        }}>
-        {`Number Of Pages : ${item.number_of_pages}`}
-      </Text>
-    </View>
+        <StatusBar backgroundColor={mainColor} translucent={false} />
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: largeFontSize,
+            alignSelf: 'center',
+            paddingVertical: smallPadding,
+          }}>
+          {item.title}
+        </Text>
+        <Text
+          style={{
+            fontSize: smallFontSize,
+            paddingVertical: smallPadding,
+          }}>
+          {`Published By : ${item.publishers.join(' and ')}`}
+        </Text>
+        <Text
+          style={{
+            fontSize: smallFontSize,
+            paddingVertical: smallPadding,
+          }}>
+          {`Published At : ${item.publish_date}`}
+        </Text>
+        <Text
+          style={{
+            fontSize: smallFontSize,
+            paddingVertical: smallPadding,
+          }}>
+          {`Number Of Pages : ${item.number_of_pages}`}
+        </Text>
+
+        <Text style={{fontSize: smallFontSize, paddingTop: smallPadding}}>
+          Authors :
+        </Text>
+        {item.authors.map(author => (
+          <Authors query={author.key} />
+        ))}
+      </View>
+    </QueryClientProvider>
   );
 };
 export default Details;
